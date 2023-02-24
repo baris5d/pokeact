33a2100fda57a1e89a81e741446565c2928b26c5
baris5d <baris@barisdede.com>
2023-02-24 23:42:33 +0300

    feat: progressive pokemon stats component

---
+++ src/components/PokemonStats/pokemon-stats.tsx
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Stats } from '../../types/pokemon';
+import {
+    kebabCaseToTitleCase,
+    maxStat,
+    useOppositeColor,
+} from '../../utils/helpers';
+
+interface StatProps {
+    stats: Stats[];
+    color: string;
+}
+
+export const PokemonDetailStats = (props: StatProps) => {
+    const { stats, color } = props;
+
+    return (
+        <div className="pokemon-detail__stats">
+            <div className="pokemon-detail__stats-wrapper">
+                {stats.map((item, index) => {
+                    return (
+                        <div className="pokemon-detail__stats-item" key={index}>
+                            <div className="pokemon-detail__stats-item-name">
+                                {kebabCaseToTitleCase(item.stat.name)}
+                            </div>
+                            <div className="pokemon-detail__stats-item-bar">
+                                <div
+                                    className="pokemon-detail__stats-item-bar-inner"
+                                    style={{
+                                        width: `${
+                                            (item.base_stat * 100) /
+                                            maxStat(item.base_stat)
+                                        }%`,
+                                        backgroundColor:
+                                            useOppositeColor(color),
+                                    }}
+                                ></div>
+                            </div>
+                            <div className="pokemon-detail__stats-item-value">
+                                {item.base_stat}
+                            </div>
+                        </div>
+                    );
+                })}
+            </div>
+        </div>
+    );
+};
