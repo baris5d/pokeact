5d8dd8c422b479ccb95c19d2ac0ba8085f49af72
baris5d <baris@barisdede.com>
2023-02-24 23:42:07 +0300

    feat: pokemon detail page

--- src/features/PokemonDetail/pokemon-detail.tsx
+++ src/features/PokemonDetail/pokemon-detail.tsx
@@ -1,12 +1,19 @@
-import React, { useEffect } from 'react';
-import { redirect, useNavigate, useParams } from 'react-router-dom';
+import Color from 'color-thief-react';
+import React from 'react';
+import { useNavigate, useParams } from 'react-router-dom';
+import { Image } from '../../components/Image/image';
+import { PokemonDetailStats } from '../../components/PokemonStats/pokemon-stats';
+import { PokemonTypes } from '../../components/PokemonTypes/pokemon-types';
+import { PokemonType, TypeIcon } from '../../components/TypeIcon/type-icon';
 import {
-    CSSTransition,
-    SwitchTransition,
-    TransitionGroup,
-} from 'react-transition-group';
-import { findProperImage } from '../../utils/helpers';
-import { usePokemon, usePokemonCollector } from '../../utils/hooks/pokemons';
+    dmToFtAndIn,
+    findProperImage,
+    formatNumber,
+    hgToKg,
+    kebabCaseToTitleCase,
+    useOppositeColor,
+} from '../../utils/helpers';
+import { usePokemonCollector } from '../../utils/hooks/pokemons';
 import './pokemon-detail.scss';
 
 export const PokemonDetail = () => {
@@ -25,36 +32,129 @@ export const PokemonDetail = () => {
     return (
         <div className="pokemon-detail">
             {Object.keys(collectedPokemon).length && (
-                <div className="pokemon-detail__wrapper">
-                    <div className="pokemon-detail__image">
-                        <img
-                            src={findProperImage(collectedPokemon.sprites)}
-                            alt={collectedPokemon.name}
-                        />
-                    </div>
-                    <div className="pokemon-detail__info">
-                        <h1>{collectedPokemon.name}</h1>
-                        <p>Height: {collectedPokemon.height}</p>
-                        <p>Weight: {collectedPokemon.weight}</p>
-                    </div>
-                    <div className="close">
-                        <button onClick={goBack}>
-                            <svg
-                                xmlns="http://www.w3.org/2000/svg"
-                                width="24"
-                                height="24"
-                                viewBox="0 0 24 24"
-                                fill="none"
-                                strokeWidth="2"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                            >
-                                <line x1="18" y1="6" x2="6" y2="18"></line>
-                                <line x1="6" y1="6" x2="18" y2="18"></line>
-                            </svg>
-                        </button>
-                    </div>
-                </div>
+                <Color
+                    src={findProperImage(collectedPokemon.sprites)}
+                    format="hex"
+                    crossOrigin="Anonymous"
+                    key={collectedPokemon.id}
+                >
+                    {({ data, loading, error }) => {
+                        if (data) {
+                            return (
+                                <div
+                                    className="pokemon-detail__wrapper"
+                                    style={{
+                                        backgroundColor: data,
+                                        color: useOppositeColor(data),
+                                    }}
+                                >
+                                    <div className="pokemon-detail__image">
+                                        <Image
+                                            src={findProperImage(
+                                                collectedPokemon.sprites,
+                                            )}
+                                            alt={collectedPokemon.name}
+                                        />
+                                    </div>
+                                    <div className="pokemon-detail__info">
+                                        <h2>
+                                            <span className="bold">
+                                                #
+                                                {formatNumber(
+                                                    collectedPokemon.id.toString(),
+                                                )}
+                                            </span>
+                                        </h2>
+                                        <h1 className="poke_name">
+                                            {kebabCaseToTitleCase(
+                                                collectedPokemon.name,
+                                            )}
+                                        </h1>
+                                        <div className="pokemon-detail__types-wrapper">
+                                            {collectedPokemon.types.map(
+                                                (item, index) => {
+                                                    return (
+                                                        <div
+                                                            className="pokemon-detail__types"
+                                                            key={index}
+                                                        >
+                                                            <TypeIcon
+                                                                type={`${
+                                                                    item.type
+                                                                        .name as PokemonType
+                                                                }`}
+                                                                className={`pokemon-detail__types-item`}
+                                                                size={128}
+                                                            />
+                                                        </div>
+                                                    );
+                                                },
+                                            )}
+                                        </div>
+                                        <div className="pokemon-detail__description">
+                                            <p>
+                                                <span className="bold">
+                                                    Height:
+                                                </span>{' '}
+                                                {dmToFtAndIn(
+                                                    collectedPokemon.height,
+                                                )}
+                                            </p>
+                                            <p>
+                                                <span className="bold">
+                                                    Weight:
+                                                </span>{' '}
+                                                {hgToKg(
+                                                    collectedPokemon.weight,
+                                                )}
+                                            </p>
+                                        </div>
+                                        <PokemonDetailStats
+                                            stats={collectedPokemon.stats}
+                                            color={data}
+                                        />
+                                        <PokemonTypes
+                                            types={collectedPokemon.types}
+                                            title="Strong"
+                                            prop="double_damage_to"
+                                        />
+                                        <PokemonTypes
+                                            types={collectedPokemon.types}
+                                            title="Weak"
+                                            prop="double_damage_from"
+                                        />
+                                    </div>
+                                    <div className="close">
+                                        <button onClick={goBack}>
+                                            <svg
+                                                xmlns="http://www.w3.org/2000/svg"
+                                                width="24"
+                                                height="24"
+                                                viewBox="0 0 24 24"
+                                                strokeWidth="2"
+                                                strokeLinecap="round"
+                                                strokeLinejoin="round"
+                                            >
+                                                <line
+                                                    x1="18"
+                                                    y1="6"
+                                                    x2="6"
+                                                    y2="18"
+                                                ></line>
+                                                <line
+                                                    x1="6"
+                                                    y1="6"
+                                                    x2="18"
+                                                    y2="18"
+                                                ></line>
+                                            </svg>
+                                        </button>
+                                    </div>
+                                </div>
+                            );
+                        }
+                    }}
+                </Color>
             )}
         </div>
     );
