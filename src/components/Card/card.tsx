ec6cb8c2b2beb38e568f627074510161fc27b7d9
baris5d <baris@barisdede.com>
2023-02-26 01:10:36 +0300

    chore: useColor hook instead of component

--- src/components/Card/card.tsx
+++ src/components/Card/card.tsx
@@ -16,57 +16,47 @@ import { Image } from '../Image/image';
 export const Card = (props: Pokemon) => {
     const { name, id, sprites } = props;
 
+    const { data, loading, error } = useColor(findProperImage(sprites), 'hex', {
+        crossOrigin: 'Anonymous',
+    });
+
     return (
         <Link to={`/pokemon/${id}`}>
             <div className={`card`}>
-                <Color
-                    src={findProperImage(sprites)}
-                    format="hex"
-                    crossOrigin="Anonymous"
-                >
-                    {({ data, loading, error }) => {
-                        if (loading)
-                            return (
-                                <div className="card__wrapper">
-                                    <PokeLoader />
-                                </div>
-                            );
-                        if (error) return <div>{JSON.stringify(error)}</div>;
-                        if (data)
-                            return (
-                                <div
-                                    className="card__wrapper"
-                                    style={{ backgroundColor: useRGBA(data) }}
-                                >
-                                    <div className="card__image-wrapper">
-                                        <Image
-                                            src={findProperImage(sprites)}
-                                            alt={name}
-                                            className="card__image"
-                                        />
-                                    </div>
-                                    <div className="card__info">
-                                        <div
-                                            className="card__title"
-                                            style={{
-                                                color: useOppositeColor(data),
-                                            }}
-                                        >
-                                            {kebabCaseToTitleCase(name)}
-                                        </div>
-                                        <div
-                                            className="card__id"
-                                            style={{
-                                                color: useOppositeColor(data),
-                                            }}
-                                        >
-                                            {formatNumber(id.toString())}
-                                        </div>
-                                    </div>
-                                </div>
-                            );
-                    }}
-                </Color>
+                {loading && <PokeLoader />}
+                {error && <div>{JSON.stringify(error)}</div>}
+                {data && (
+                    <div
+                        className="card__wrapper"
+                        style={{ backgroundColor: useRGBA(data) }}
+                    >
+                        <div className="card__image-wrapper">
+                            <Image
+                                src={findProperImage(sprites)}
+                                alt={name}
+                                className="card__image"
+                            />
+                        </div>
+                        <div className="card__info">
+                            <div
+                                className="card__title"
+                                style={{
+                                    color: useOppositeColor(data),
+                                }}
+                            >
+                                {kebabCaseToTitleCase(name)}
+                            </div>
+                            <div
+                                className="card__id"
+                                style={{
+                                    color: useOppositeColor(data),
+                                }}
+                            >
+                                {formatNumber(id.toString())}
+                            </div>
+                        </div>
+                    </div>
+                )}
             </div>
         </Link>
     );
--- src/features/PokemonDetail/pokemon-detail.tsx
+++ src/features/PokemonDetail/pokemon-detail.tsx
@@ -1,4 +1,4 @@
-import Color from 'color-thief-react';
+import Color, { useColor } from 'color-thief-react';
 import React from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { Image } from '../../components/Image/image';
@@ -25,147 +25,110 @@ export const PokemonDetail = () => {
         'abilities.ability',
     ]);
 
+    const { data, loading, error } = useColor(
+        findProperImage(collectedPokemon.sprites),
+        'hex',
+        {
+            crossOrigin: 'Anonymous',
+        },
+    );
+
     const goBack = () => {
         navigate('/');
     };
 
     return (
         <div className="pokemon-detail">
-            {Object.keys(collectedPokemon).length && (
-                <Color
-                    src={findProperImage(collectedPokemon.sprites)}
-                    format="hex"
-                    crossOrigin="Anonymous"
-                    key={collectedPokemon.id}
-                >
-                    {({ data, loading, error }) => {
-                        if (data) {
-                            return (
-                                <div
-                                    className="pokemon-detail__wrapper"
-                                    style={{
-                                        backgroundColor: data,
-                                        color: useOppositeColor(data),
-                                    }}
-                                >
-                                    <div className="pokemon-detail__image">
-                                        <Image
-                                            src={findProperImage(
-                                                collectedPokemon.sprites,
-                                            )}
-                                            alt={collectedPokemon.name}
-                                        />
-                                    </div>
-                                    <div className="pokemon-detail__info">
-                                        <h2>
-                                            <span className="bold">
-                                                #
-                                                {formatNumber(
-                                                    collectedPokemon.id.toString(),
-                                                )}
-                                            </span>
-                                        </h2>
-                                        <h1 className="poke_name">
-                                            {kebabCaseToTitleCase(
-                                                collectedPokemon.name,
-                                            )}
-                                        </h1>
-                                        <div className="pokemon-detail__types-wrapper">
-                                            {collectedPokemon.types.map(
-                                                (item, index) => {
-                                                    return (
-                                                        <div
-                                                            className="pokemon-detail__types"
-                                                            key={index}
-                                                        >
-                                                            <TypeIcon
-                                                                type={`${
-                                                                    item.type
-                                                                        .name as PokemonType
-                                                                }`}
-                                                                className={`pokemon-detail__types-item`}
-                                                                size={128}
-                                                            />
-                                                        </div>
-                                                    );
-                                                },
-                                            )}
-                                        </div>
-                                        <div className="pokemon-detail__description">
-                                            <p>
-                                                <span className="bold">
-                                                    Height:
-                                                </span>{' '}
-                                                {dmToFtAndIn(
-                                                    collectedPokemon.height,
-                                                )}
-                                            </p>
-                                            <p>
-                                                <span className="bold">
-                                                    Weight:
-                                                </span>{' '}
-                                                {hgToKg(
-                                                    collectedPokemon.weight,
-                                                )}
-                                            </p>
-                                        </div>
-                                        <PokemonDetailStats
-                                            stats={collectedPokemon.stats}
-                                            color={data}
-                                        />
-
-                                        {[
-                                            'double_damage_to',
-                                            'double_damage_from',
-                                        ].map((prop, index) => {
-                                            return (
-                                                <PokemonTypes
-                                                    types={
-                                                        collectedPokemon.types
-                                                    }
-                                                    title={
-                                                        prop ===
-                                                        'double_damage_to'
-                                                            ? 'Strong'
-                                                            : 'Weak'
-                                                    }
-                                                    prop={prop}
-                                                    key={index}
-                                                />
-                                            );
-                                        })}
-                                    </div>
-                                    <div className="close">
-                                        <button onClick={goBack}>
-                                            <svg
-                                                xmlns="http://www.w3.org/2000/svg"
-                                                width="24"
-                                                height="24"
-                                                viewBox="0 0 24 24"
-                                                strokeWidth="2"
-                                                strokeLinecap="round"
-                                                strokeLinejoin="round"
-                                            >
-                                                <line
-                                                    x1="18"
-                                                    y1="6"
-                                                    x2="6"
-                                                    y2="18"
-                                                ></line>
-                                                <line
-                                                    x1="6"
-                                                    y1="6"
-                                                    x2="18"
-                                                    y2="18"
-                                                ></line>
-                                            </svg>
-                                        </button>
-                                    </div>
-                                </div>
-                            );
-                        }
+            {Object.keys(collectedPokemon).length && data && (
+                <div
+                    className="pokemon-detail__wrapper"
+                    style={{
+                        backgroundColor: data,
+                        color: useOppositeColor(data),
                     }}
-                </Color>
+                >
+                    <div className="pokemon-detail__image">
+                        <Image
+                            src={findProperImage(collectedPokemon.sprites)}
+                            alt={collectedPokemon.name}
+                        />
+                    </div>
+                    <div className="pokemon-detail__info">
+                        <h2>
+                            <span className="bold">
+                                #{formatNumber(collectedPokemon.id.toString())}
+                            </span>
+                        </h2>
+                        <h1 className="poke_name">
+                            {kebabCaseToTitleCase(collectedPokemon.name)}
+                        </h1>
+                        <div className="pokemon-detail__types-wrapper">
+                            {collectedPokemon.types.map((item, index) => {
+                                return (
+                                    <div
+                                        className="pokemon-detail__types"
+                                        key={index}
+                                    >
+                                        <TypeIcon
+                                            type={`${
+                                                item.type.name as PokemonType
+                                            }`}
+                                            className={`pokemon-detail__types-item`}
+                                            size={128}
+                                        />
+                                    </div>
+                                );
+                            })}
+                        </div>
+                        <div className="pokemon-detail__description">
+                            <p>
+                                <span className="bold">Height:</span>{' '}
+                                {dmToFtAndIn(collectedPokemon.height)}
+                            </p>
+                            <p>
+                                <span className="bold">Weight:</span>{' '}
+                                {hgToKg(collectedPokemon.weight)}
+                            </p>
+                        </div>
+                        <PokemonDetailStats
+                            stats={collectedPokemon.stats}
+                            color={data}
+                        />
+
+                        {['double_damage_to', 'double_damage_from'].map(
+                            (prop, index) => {
+                                return (
+                                    <PokemonTypes
+                                        types={collectedPokemon.types}
+                                        title={
+                                            prop === 'double_damage_to'
+                                                ? 'Strong'
+                                                : 'Weak'
+                                        }
+                                        prop={prop}
+                                        key={index}
+                                    />
+                                );
+                            },
+                        )}
+                    </div>
+                    <div className="close">
+                        <button onClick={goBack}>
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                strokeWidth="2"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                            >
+                                <line x1="18" y1="6" x2="6" y2="18"></line>
+                                <line x1="6" y1="6" x2="18" y2="18"></line>
+                            </svg>
+                        </button>
+                    </div>
+                </div>
             )}
         </div>
     );
